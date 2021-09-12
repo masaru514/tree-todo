99e69a683feffaa6de522419c8be22712eac6aac
masaru514 <415urasam@gmail.com>
2021-09-12 16:44:21 +0900

    update: refactor, add func, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,17 @@
-import React, { useState } from 'react';
 import './App.css';
-import { Box, Checkbox, Input } from "@chakra-ui/react"
-import { cond } from 'lodash';
-
-type TodoListObjectProps = {
-  text: string
-}
+import { Box, Input } from "@chakra-ui/react"
+import SubTask from './components/SubTask';
+import useTask from './hooks';
 
 function App() {
-  const [list, setList] = useState<TodoListObjectProps[]>([])
-  const [currentTextFieldValue, setCurrentTextFieldValue] = useState('')
-  const enterHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== 'Enter' || currentTextFieldValue === '') return
-    list ? setList([...list, { text: currentTextFieldValue }]) : setList([{ text: currentTextFieldValue }])
-    setCurrentTextFieldValue('')
-  }
-
-  // サブタスクのテキストフィールドの存在を監視する
-  const [watch, setWatch] = useState(false)
-
-  const SubTask = (item: any, index: number) => {
-    const [create, setCreate] = useState(false)
-    const [subList, setSubList] = useState<TodoListObjectProps[]>([])
-    const [currentSubTextFieldValue, setCurrentSubTextFieldValue] = useState('')
-    const createSubtask = () => {
-      setCreate(true)
-    }
-
-    const enterSubTaskHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-      if (e.key !== 'Enter' || currentSubTextFieldValue === '') return
-      list ? setSubList([...subList, { text: currentSubTextFieldValue }]) : setSubList([{ text: currentSubTextFieldValue }])
-      setCurrentSubTextFieldValue('')
-    }
-
-    const blurHandler = () => {
-      // if (currentSubTextFieldValue === '') return
-      console.log('ee?')
-      subList ? setSubList([...subList, { text: currentSubTextFieldValue }]) : setSubList([{text: currentSubTextFieldValue}])
-      setCurrentSubTextFieldValue('')
-      setCreate(false)
-    }
-
-    return (
-      <Box pl={8} pt={4}>
-        <Box display="flex" onClick={() => createSubtask()}>
-          <Checkbox size="lg" mr={4} />
-          <div key={index}>{item.text}</div>
-        </Box>
-        {subList.map(item => {
-          return (
-            <div>
-              {item.text}
-            </div>
-          )
-        })}
-        {create && <Input value={currentSubTextFieldValue} onChange={(e) => setCurrentSubTextFieldValue(e.target.value)} onKeyPress={(e) => enterSubTaskHandler(e)} onBlur={() => blurHandler()}  ml={8} mt={4} display="inline-block" />}
-      </Box>
-    )
-  }
-
+  const { list, enterHandler, currentTextFieldValue, setCurrentTextFieldValue } = useTask()
   return (
     <div className="App">
       <main>
         <ul>
           {list.map((item, index) => {
               return (
-                <SubTask {...item} {...index} />
+                <SubTask {...item} {...index} {...list} />
               )
             }
           )}
@@ -73,7 +19,7 @@ function App() {
         </ul>
 
         <Box display="flex" alignItems="center" mt={10} mx={8}>
-          <Input value={currentTextFieldValue} display="inline-block" placeholder="baxic" onChange={(e) => setCurrentTextFieldValue(e.target.value)} onKeyPress={(e) => enterHandler(e)} />
+          <Input value={currentTextFieldValue} display="inline-block" placeholder="新しいタスクを作成" onChange={(e) => setCurrentTextFieldValue(e.target.value)} onKeyPress={(e) => enterHandler(e)} />
         </Box>
       </main>
     </div>
